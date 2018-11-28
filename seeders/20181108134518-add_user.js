ecfe5e6afa891a27438be7b05fb6ffa8733b2b5e
Code Handbook <codehandbook@gmail.com>
2018-11-28 21:16:17 +0530

    Update 20181108134518-add_user.js

--- seeders/20181108134518-add_user.js
+++ seeders/20181108134518-add_user.js
@@ -6,16 +6,18 @@ module.exports = {
       id: 1,
       firstName: 'Jay',
       lastName: 'Raj',
-      email: 'jay3dec@gmail.com',
+      email: 'jay@gmail.com',
       role: 1,
+      password: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
       id: 2,
       firstName: 'Roy',
       lastName: 'Agasthyan',
-      email: 'royagasthyan@gmail.com',
+      email: 'roy@gmail.com',
       role: 2,
+      password: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
@@ -24,6 +26,7 @@ module.exports = {
       lastName: 'Samuel',
       email: 'jacksamuel@gmail.com',
       role: 3,
+      password: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }], {})
